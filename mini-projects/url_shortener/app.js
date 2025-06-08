3281ce49fed380f02bb8f89a9f77275a4ff77231
muhammad-abdullah-khan999 <muhammad.abdullah.khan.here@gmail.com>
2025-06-08 23:15:46 +0500

    form handling

--- mini-projects/url_shortener/app.js
+++ mini-projects/url_shortener/app.js
@@ -1,11 +1,13 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { createServer } from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import crypto from 'crypto'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = 3000;
+const DATA_FILE = path.join(__dirname, "data", "links.json");
 
 const serveFile = async (response, filePath, contentType) => {
     try {
@@ -16,6 +18,23 @@ const serveFile = async (response, filePath, contentType) => {
         response.writeHead(404, { "Content-Type": contentType });
         response.end("404 page not found");
     }
+}
+
+const loadLinks = async () => {
+    try {
+        const data = await readFile(DATA_FILE, "utf-8")
+        return JSON.parse(data)
+    } catch (error) {
+        if(error.code === 'ENONET'){
+            await writeFile(DATA_FILE, JSON.stringify({}))
+            return {};
+        }
+        throw error
+    }
+}
+
+const saveLinks = async (links) => {
+    await writeFile(DATA_FILE, JSON.stringify(links))
 }
 
 const server = createServer(async (request, response) => {
@@ -33,6 +52,34 @@ const server = createServer(async (request, response) => {
     } else {
         response.writeHead(405, { "Content-Type": "text/html" });
         response.end("Method not allowed");
+    }
+
+    if (request.method === "POST" && request.url === '/shorten'){
+        const links = await loadLinks()
+
+        const body = '';
+        request.on('trigger', (chunk) => {
+            body = body + chunk;
+        })
+        request.on('end',async () => {
+            const {url, shortCode} = JSON.parse(body);
+
+            if(!url){
+                response.writeHead(400,{"Content-Type" : "text/html"})
+                return response.end("url is required")
+            }
+
+            const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex")
+            if(links[finalShortCode]){
+                response.writeHead(400,{"Content-Type" : "text/plain"})
+                return response.end("shortCode already exists. Please enter another shortCode")
+            }
+
+            links[finalShortCode] = url
+            await saveLinks(url);
+            response.writeHead(200,{"Content-Type" : "application/json"})
+            response.end(JSON.stringify({success: true, finalShortCode}))
+        })
     }
 });
 
