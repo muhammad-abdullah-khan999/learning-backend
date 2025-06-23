3b30adb9c1aab06309282739c2857aa377e0aee9
muhammad-abdullah-khan999 <muhammad.abdullah.khan.here@gmail.com>
2025-06-23 18:24:29 +0500

    express router added to url_shortener

--- mini-projects/url_shortener/app.js
+++ mini-projects/url_shortener/app.js
@@ -1,95 +1,13 @@
-import { readFile, writeFile, mkdir } from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import crypto from 'crypto';
 import express from 'express';
+import { shortenerRoutes } from'./routes/shortener.routes.js'
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const port = 3000;
-const DATA_FILE = path.join(__dirname, "data", "links.json");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const loadLinks = async () => {
-    try {
-        const data = await readFile(DATA_FILE, "utf-8");
-        return JSON.parse(data);
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            await mkdir(path.dirname(DATA_FILE), { recursive: true });
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {};
-        }
-        throw error;
-    }
-};
-
-const saveLinks = async (links) => {
-    await writeFile(DATA_FILE, JSON.stringify(links, null, 2)); 
-};
-
-app.get("/", async (req, res) => {
-    try {
-        const file = await readFile(path.join(__dirname, "views", "index.html"));
-        const links = await loadLinks();
-        const content = file.toString().replaceAll("{{ shortened_urls }}",
-            Object.entries(links).map(([shortCode, url]) => {
-                return `
-                    <li>
-                        <a href="/${shortCode}" target="_blank">${req.get("host")}/${shortCode}</a>
-                        <button class="copy-btn" onclick="navigator.clipboard.writeText('${req.get("host")}/${shortCode}')">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v16h14c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 18H8V7h11v16z"/>
-                            </svg>
-                        </button>
-                    </li>
-                `;
-            }).join("")
-        );
-        return res.send(content);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Invalid server error");
-    }
-});
-
-app.post("/", async (req, res) => {
-    try {
-        const { url, shortCode } = req.body;
-        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-        const links = await loadLinks();
-
-        if (links[finalShortCode]) {
-            return res.status(400).send("Short code already exists. Please enter another short code.");
-        }
-
-        links[finalShortCode] = url;
-        await saveLinks(links);
-
-        return res.redirect("/");
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Invalid server error");
-    }
-});
-
-app.get("/:shortCode", async (req, res) => {
-    try {
-        const { shortCode } = req.params;
-        const links = await loadLinks();
-
-        if (!links[shortCode]) return res.status(404).send("404 error occurred");
-
-        return res.redirect(links[shortCode]);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-});
+app.use(shortenerRoutes)
 
 app.listen(port, () => {
     console.log(`Server Running on http://localhost:${port}`);
