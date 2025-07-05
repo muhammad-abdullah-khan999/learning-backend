402a4197ea2f3dd75aa097eace694410ad5c686c
muhammad-abdullah-khan999 <muhammad.abdullah.khan.here@gmail.com>
2025-07-05 17:04:39 +0500

    added prisma orm to url_shortener

--- mini-projects/url_shortener/config/db-client.js
+++ mini-projects/url_shortener/config/db-client.js
@@ -1,9 +1,9 @@
-import mysql from 'mysql2/promise'
-import { env } from './env.js'
+// import mysql from 'mysql2/promise'
+// import { env } from './env.js'
 
-export const db = await mysql.createConnection({
-    host: env.DATABASE_HOST,
-    user: env.DATABASE_USER,
-    database: env.DATABASE_NAME,
-    password: env.DATABASE_PASSWORD
-})
+// export const db = await mysql.createConnection({
+//     host: env.DATABASE_HOST,
+//     user: env.DATABASE_USER,
+//     database: env.DATABASE_NAME,
+//     password: env.DATABASE_PASSWORD
+// })
--- mini-projects/url_shortener/models/shortener.model.js
+++ mini-projects/url_shortener/models/shortener.model.js
@@ -1,22 +1,22 @@
-import { db } from "../config/db-client.js";
+// import { db } from "../config/db-client.js";
 
-export const loadLinks = async () => {
-  const [rows] = await db.execute('select * from short_links');
-  return rows;
-}
+// export const loadLinks = async () => {
+//   const [rows] = await db.execute('select * from short_links');
+//   return rows;
+// }
 
-export const insertShortLink = async ({url, shortCode}) => {
-    const [result] = await db.execute('insert into short_links(short_code, url) values(?,?)', [shortCode,url])
-    return result;
-}
+// export const insertShortLink = async ({url, shortCode}) => {
+//     const [result] = await db.execute('insert into short_links(short_code, url) values(?,?)', [shortCode,url])
+//     return result;
+// }
 
-export const getLinkByShortCode = async (shortcode) => {
-    const [rows] = await db.execute('select * from short_links where short_code = ?', [shortcode]);
+// export const getLinkByShortCode = async (shortcode) => {
+//     const [rows] = await db.execute('select * from short_links where short_code = ?', [shortcode]);
 
-    if (rows.length > 0) {
-        return rows [0];
-    } else {
-        return null
-    }
-}
+//     if (rows.length > 0) {
+//         return rows [0];
+//     } else {
+//         return null
+//     }
+// }
 
